3d7ec9edb84ba41ecc6c5577b72b4ec02939749a
aguilarjordy <jordyaguilarm@gmail.com>
2025-10-24 02:58:32 -0500

    oi waa

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error("❌ Variables de entorno no detectadas:", {
+    supabaseUrl,
+    supabaseKey,
+  });
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
